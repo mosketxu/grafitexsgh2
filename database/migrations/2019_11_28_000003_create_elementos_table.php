144fe129c4b34b3bb38a857f3e392a339c19a839
Alexander Arregui <mosketxu@gmail.com>
2023-04-13 10:53:04 +0200

    sigo con migraciones

--- database/migrations/2019_11_28_000003_create_elementos_table.php
+++ database/migrations/2019_11_28_000003_create_elementos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateElementsTable extends Migration
+class CreateElementosTable extends Migration
 {
     /**
      * Run the migrations.
