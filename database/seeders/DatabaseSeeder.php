b532be8dae7be966d96609b0c80116b0b37e3fc6
Alexander Arregui <mosketxu@gmail.com>
2023-04-13 11:29:00 +0200

    seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(RoleSeeder::class);
-        // $this->call(UsersTableSeeder::class);
-        // $this->call(PermissionSeeder::class);
-        // $this->call(ProvinciasTableSeeder::class);
+        $this->call(RoleSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(PermissionSeeder::class);
+        $this->call(ProvinciasTableSeeder::class);
         $this->call(StoresTableSeeder::class);
         $this->call(AddressesTableSeeder::class);
         // $this->call(ElementsTableSeeder::class);
