73006de8d9c0a010992b9bee3ac81e9a245bab31
Alexander Arregui <mosketxu@gmail.com>
2023-04-13 12:17:37 +0200

    tarfifas database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,12 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(RoleSeeder::class);
-        $this->call(UsersTableSeeder::class);
-        $this->call(PermissionSeeder::class);
-        $this->call(ProvinciasTableSeeder::class);
-        $this->call(StoresTableSeeder::class);
-        $this->call(AddressesTableSeeder::class);
+        // $this->call(RoleSeeder::class);
+        // $this->call(UsersTableSeeder::class);
+        // $this->call(PermissionSeeder::class);
+        // $this->call(ProvinciasTableSeeder::class);
+        // $this->call(StoresTableSeeder::class);
+        // $this->call(AddressesTableSeeder::class);
         // $this->call(ElementsTableSeeder::class);
         // $this->call(CampaignsTableSeeder::class);
         // $this->call(StoreElementsTableSeeder::class);
@@ -31,9 +31,9 @@ class DatabaseSeeder extends Seeder
         // $this->call(StoreconceptsTableSeeder::class);
         // $this->call(AreasTableSeeder::class);
         // $this->call(CountriesTableSeeder::class);
-        // $this->call(TarifaSeeder::class);
+        $this->call(TarifaSeeder::class);
         // $this->call(MaterialMedidasSeeder::class);
-        // $this->call(TarifaFamiliasSeeder::class);
+        $this->call(TarifaFamiliasSeeder::class);
         // $this->call(Usuariostiendas::class);
     }
 }
