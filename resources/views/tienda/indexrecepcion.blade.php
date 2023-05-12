df5c86049276600e6621b0c08f44cc2ddb4f4bb4
Alexander Arregui <mosketxu@gmail.com>
2023-05-12 20:09:04 +0200

    p1

--- resources/views/tienda/indexrecepcion.blade.php
+++ resources/views/tienda/indexrecepcion.blade.php
@@ -2,14 +2,14 @@
     <x-slot name="header">
         <div class="flex">
             <div class="w-full">
-                <h2 class="text-xl font-semibold leading-tight text-gray-800">Campañas de la tienda <span class="text-blue-600"> {{ $store->id }} -{{ $store->name }} </span></h2>
+                {{-- <h2 class="text-xl font-semibold leading-tight text-gray-800">Campañas de la tienda <span class="text-blue-600"> {{ $store->id }} -{{ $store->name }} </span></h2> --}}
             </div>
         </div>
     </x-slot>
     <div class="p-2">
         <div class="max-w-full mx-auto">
             <div class="overflow-hidden bg-white shadow-xl sm:rounded-lg">
-                {{-- @include('tienda.recepcion') --}}
+                @include('tienda.recepcion')
             </div>
         </div>
     </div>
