e260847dcddbefb1cc4fa79f581fb27cf2db91b4
Alexander Arregui <mosketxu@gmail.com>
2023-05-12 19:11:30 +0200

    vista ents mobile

--- resources/views/livewire/entidad/ents.blade.php
+++ resources/views/livewire/entidad/ents.blade.php
@@ -11,37 +11,37 @@
             <div class="flex-col space-y-4">
                 <div>
                     <div class="flex w-full py-2 pl-2 text-sm text-gray-500 bg-blue-100 rounded-t-md">
-                        <div class="w-3/12 font-light" ><input type="text" class="w-full py-0 text-sm text-gray-500 bg-blue-100 border-0 rounded-md" value= "{{ __('Nombre') }}" /></div>
-                        <div class="w-1/12 font-light" ><input type="text" class="w-full text-center py-0 text-sm text-gray-500 bg-blue-100 border-0 rounded-md" value= "{{ __('Montador') }}" /></div>
-                        <div class="w-3/12 font-light" ><input type="text" class="w-full py-0 text-sm text-gray-500 bg-blue-100 border-0 rounded-md" value= "{{ __('Ciudad') }}" /></div>
-                        <div class="w-2/12 font-light" ><input type="text" class="w-full py-0 text-sm text-gray-500 bg-blue-100 border-0 rounded-md" value= "{{ __('Provincia') }}" /></div>
-                        <div class="w-2/12 font-light" ><input type="text" class="w-full py-0 text-sm text-gray-500 bg-blue-100 border-0 rounded-md" value= "{{ __('Area') }} " /></div>
-                        <div class="w-1/12 " ></div>
+                        <div class="w-4/12 font-light md:w-3/12" ><input type="text" class="w-full py-0 text-sm text-gray-500 bg-blue-100 border-0 rounded-md" value= "{{ __('Nombre') }}" /></div>
+                        <div class="w-1/12 font-light " ><input type="text" class="w-full py-0 text-sm text-center text-gray-500 bg-blue-100 border-0 rounded-md" value= "{{ __('Montador') }}" /></div>
+                        <div class="hidden font-light md:flex md:w-3/12" ><input type="text" class="w-full py-0 text-sm text-gray-500 bg-blue-100 border-0 rounded-md" value= "{{ __('Ciudad') }}" /></div>
+                        <div class="w-3/12 font-light md:w-2/12" ><input type="text" class="w-full py-0 text-sm text-gray-500 bg-blue-100 border-0 rounded-md" value= "{{ __('Provincia') }}" /></div>
+                        <div class="w-2/12 font-light " ><input type="text" class="w-full py-0 text-sm text-gray-500 bg-blue-100 border-0 rounded-md" value= "{{ __('Area') }} " /></div>
+                        <div class="w-2/12 md:w-1/12" ></div>
                     </div>
                     <div>
                         @forelse ($entidades as $entidad)
                             <div class="flex items-center w-full pl-2 text-sm border-t-0 border-y" wire:loading.class.delay="opacity-50">
-                                <div class="w-3/12">
+                                <div class="w-4/12 md:w-3/12">
                                     <input type="text" class="w-full text-sm font-thin text-gray-500 border-0 rounded-md" value="{{ $entidad->entidad }}" readonly/>
                                 </div>
-                                <div class="w-1/12 text-center">
+                                <div class="w-1/12 text-center ">
                                     @if($entidad->montador=='1')
-                                        <x-icon.circle-check-a class="text-green-600  hover:text-green-600 hover:hover:scale-100"/>
+                                        <x-icon.circle-check-a class="text-green-600 hover:text-green-600 hover:hover:scale-100"/>
                                     @endif
                                 </div>
-                                <div class="w-3/12">
+                                <div class="hidden md:w-3/12 md:flex">
                                     <input type="text" class="w-full text-sm font-thin text-gray-500 border-0 rounded-md" value="{{ $entidad->localidad }}"  readonly/>
                                 </div>
-                                <div class="w-2/12">
+                                <div class="w-3/12 md:w-2/12">
                                     <input type="text" class="w-full text-sm font-thin text-gray-500 border-0 rounded-md" value="{{ $entidad->provinci->provincia ?? '-'}}"  readonly/>
                                 </div>
-                                <div class="w-2/12">
+                                <div class="w-2/12 ">
                                     <input type="text" class="w-full text-sm font-thin text-gray-500 border-0 rounded-md" value="{{ $entidad->entidadarea->area ?? '-' }}" readonly/>
                                 </div>
-                                <div class="w-1/12 justify-between flex">
-                                    <x-icon.edit-a href="{{ route('entidad.edit',$entidad) }}" class="w-6"  title="Editar"/>
-                                    <x-icon.usergroup-a href="{{ route('entidad.contactos',$entidad) }}" class="w-6"  title="Contactos"/>
-                                    <x-icon.trash-a class="w-5 text-red-500" wire:click.prevent="delete({{ $entidad->id }})" onclick="confirm('¿Estás seguro?') || event.stopImmediatePropagation()"/>
+                                <div class="flex justify-between w-2/12 md:w-1/12">
+                                    <div class="w-5"><x-icon.edit-a href="{{ route('entidad.edit',$entidad) }}" class="w-6"  title="Editar"/></div>
+                                    <div class="w-5"><x-icon.usergroup-a href="{{ route('entidad.contactos',$entidad) }}" class="w-6"  title="Contactos"/></div>
+                                    <div class="w-5"><x-icon.trash-a class="w-5 text-red-500" wire:click.prevent="delete({{ $entidad->id }})" onclick="confirm('¿Estás seguro?') || event.stopImmediatePropagation()"/></div>
                                 </div>
                             </div>
                         @empty
